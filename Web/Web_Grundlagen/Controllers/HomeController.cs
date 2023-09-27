88d540a1c6c1b453da21a1abb47c141dfa359179
stefanmallaun <stmallaun@tsn.at>
2023-09-27 10:36:58 +0200

    StartProjekt

---
+++ Web/Web_Grundlagen/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Web_Grundlagen.Models;
+
+namespace Web_Grundlagen.Controllers {
+    public class HomeController : Controller {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger) {
+            _logger = logger;
+        }
+
+        public IActionResult Index() {
+            return View();
+        }
+
+        public IActionResult Privacy() {
+            return View();
+        }
+
+        public IActionResult Test() {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
